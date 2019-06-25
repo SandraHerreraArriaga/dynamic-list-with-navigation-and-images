6347a779fb96c477de0fd120014bbc3ec18e1804
Sandra Herrera <sandra.herrera@edisoneffect.tech>
2019-06-25 11:23:08 -0500

    Initial Commit

---
+++ ProjectSwiftUI-1/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ProjectSwiftUI-1
+//
+//  Created by Sandra Herrera on 25/06/19.
+//  Copyright © 2019 Edison Effect. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ ProjectSwiftUI-1Tests/ProjectSwiftUI_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ProjectSwiftUI_1Tests.swift
+//  ProjectSwiftUI-1Tests
+//
+//  Created by Sandra Herrera on 25/06/19.
+//  Copyright © 2019 Edison Effect. All rights reserved.
+//
+
+import XCTest
+@testable import ProjectSwiftUI_1
+
+class ProjectSwiftUI_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
